f75c9b1b813ac18f2be3a3100c2c0c728b9a159d
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-06-19 13:02:53 +0530

    Day 03

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 * https://peps.python.org/pep-0008/
 * https://peps.python.org/pep-0020/
 
+### Reg Ex
+* https://docs.python.org/3/library/re.html
+* https://docs.python.org/3/howto/regex.html
 
 
 Feedback - "https://forms.gle/ExathWUWX5nVfvm16"
---
+++ _17_RegEx_Basics.py
@@ -0,0 +1,14 @@
+import re
+
+'''
+string = "The Euro STOXX 600 index, which tracks all stock markets across Europe \
+    including the FTSE, fell by 11.48% – the worst day since it launched in 1998. \
+    The panic selling prompted by the coronavirus has wiped £2.7tn off the value \
+    of STOXX 600 shares since its all-time peak on 19 February."
+'''
+
+string = "The Euro STOXX 600 index, which tracks all stock markets across Europe including the FTSE, fell by 11.48% – the worst day since it launched in 1998. The panic selling prompted by the coronavirus has wiped £2.7tn off the value of STOXX 600 shares since its all-time peak on 19 February."
+
+
+pattern = r"c:\Users\temp\newfile.txt"
+print(pattern)
